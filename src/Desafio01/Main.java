ce40f004e51e08f703f5a9ed7c99dd10cb2a9cdf
talitaambrozio <talitalmeidas@gmail.com>
2022-05-31 18:04:31 -0300

    atualizacao 3

--- src/Desafio01/Main.java
+++ src/Desafio01/Main.java
@@ -6,6 +6,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a expressão: ");
+        String expressao = entrada.nextLine();
+
+
 
 
 
